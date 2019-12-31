5ce28e4db9721c4dc39a9e8a707735f4551fe256
brtvsk <vadim.nakitnyak@gmail.com>
2019-12-31 21:58:16 +0200

    add NoteModification activity and fragment

---
+++ app/src/main/java/com/brtvsk/noter/NoteModificationActivity.java
@@ -0,0 +1,14 @@
+package com.brtvsk.noter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoteModificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_modification);
+    }
+}
